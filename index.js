9fc8035c5dd2403089a741ddf813acf558c5307e
Javi Puche <javierpuchevictoria@gmail.com>
2019-07-04 08:28:39 +0200

    feat: Better error messages

--- index.js
+++ index.js
@@ -7,29 +7,29 @@ const errorMessage = require('./lib/errorMessage')
 const isHandlebars = (file) => allowedExtensions.indexOf(extname(file.path).split('.').pop()) >= 0
 
 const maquetus = (options) => {
-    if (!options) {
-        return errorMessage('Configure Maquetus options please.')
-    }
-
-    if (options && !options.layouts) {
-        return errorMessage('You must specify a directory for layouts.')
-    }
-
-    if (options && !options.partials) {
-        return errorMessage('You must specify a directory for partials.')
-    }
-
     return through.obj((file, encoding, callback) => {
+        if (!options) {
+            return callback(errorMessage('Maquetus error: Configure Maquetus options please.'))
+        }
+
+        if (options && !options.layouts) {
+            return callback(errorMessage('Maquetus error: You must specify a directory for layouts.'))
+        }
+
+        if (options && !options.partials) {
+            return callback(errorMessage('Maquetus error: You must specify a directory for partials.'))
+        }
+
         if (file.isNull()) {
             return callback(null, file)
         }
 
         if (file.isStream()) {
-            return callback(errorMessage('Streams not supported!', file))
+            return callback(errorMessage('Maquetus error: Streams not supported!'))
         }
 
         if (!isHandlebars(file)) {
-            return callback(errorMessage(`File extension not supported! Allowed extensions: ${allowedExtensions.join(', ')}.`, file))
+            return callback(errorMessage(`Maquetus error: File extension not supported! Allowed extensions: ${allowedExtensions.join(', ')}.`))
         }
 
         try {
@@ -37,7 +37,7 @@ const maquetus = (options) => {
             file.path = file.path.replace(extname(file.path), (options.ext || '.html'))
             return callback(null, file)
         } catch (err) {
-            return callback(errorMessage(err, file))
+            return callback(errorMessage(err))
         }
     })
 }
---
+++ tpl/errorTpl.js
@@ -0,0 +1,48 @@
+const errorTpl = (err) => `
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Maquetus error</title>
+        <style>
+            * {
+                box-sizing: border-box;
+            }
+
+            body {
+                margin: 0;
+                background-color: #2b2b2b;
+                color: #fff;
+                font-family: Helvetica, sans-serif;
+            }
+
+            h1 {
+                font-size: 20px;
+                font-weight: 400;
+            }
+
+            .container {
+                padding: 24px;
+            }
+
+            pre {
+                color: #ccc;
+                background-color: #000;
+                padding: 16px;
+                max-width: 100%;
+                overflow-x: auto;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Maquetus error:</h1>
+            <pre><code>${err}</code></pre>
+        </div>
+    </body>
+</html>
+`
+
+module.exports = errorTpl
